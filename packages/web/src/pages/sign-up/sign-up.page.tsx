d65fb57a685a61257a638c6c6a6603d50afd188e
ShutSasha <sanyashut159@gmail.com>
2024-10-18 17:08:20 +0300

    feat: add auth-input component (#20)

--- packages/web/src/pages/sign-up/sign-up.page.tsx
+++ packages/web/src/pages/sign-up/sign-up.page.tsx
@@ -1,10 +1,11 @@
+import { AuthInput } from '@shared/components/auth-input/auth-input.component'
 import { AuthLayout } from '@shared/layouts/auth/auth.layout'
 import { FC } from 'react'
 
 export const SignUp: FC = () => {
   return (
     <AuthLayout>
-      <p>children here</p>
+      <AuthInput label="Username" />
     </AuthLayout>
   )
 }
