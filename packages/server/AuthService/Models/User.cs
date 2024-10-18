9d24296a9eccf9fc9a6076324256335a345c6068
Artem <118220003+guntersteam@users.noreply.github.com>
2024-10-18 21:08:01 +0300

    feat: added user model (#15)

---
+++ packages/server/AuthService/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace AuthService.Models;
+
+[BsonIgnoreExtraElements]
+public class User
+{
+   [BsonId]
+   [BsonRepresentation(BsonType.ObjectId)]
+   public string id { get; set; }
+   public string name { get; set; }
+   public string userName { get; set; }
+   public string avatar { get; set; }
+   [BsonRepresentation(BsonType.DateTime)]
+   public DateTime createdAt { get; set; } = DateTime.Now;
+   public int devPoints { get; set; } = 0;
+   public string activationLink { get; set; }
+   public bool isActivated { get; set; }
+}
