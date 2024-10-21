4f9afbbd838813a38d3b835b3fa07dc04a297281
Artem <temaprgod8@gmail.com>
2024-10-21 18:57:23 +0300

    fix: [16] move html message

--- packages/server/AuthService/Services/MailService.cs
+++ packages/server/AuthService/Services/MailService.cs
@@ -20,49 +20,11 @@ public class MailService
       var from = new EmailAddress(_senderDataSettings.SenderEmail,"DevHub");
       var subject = "Activation link DevHub";
       var to = new EmailAddress(recieverEmail);
+      var templatePath = Path.Combine(Directory.GetCurrentDirectory(), "Templates", "EmailTemplate.html");
+      var htmlContent = await File.ReadAllTextAsync(templatePath);
+      
+      htmlContent = htmlContent.Replace("{{code}}", code);
       var plainTextContent = "";
-      var htmlContent = $@"<html>
-      <head>
-        <style>
-          .container {{
-            text-align: center;
-            padding: 50px 20px;
-          }}
-          .content {{
-            background-color: #2A2A2D;
-            padding: 40px;
-            border-radius: 10px;
-            display: inline-block;
-            max-width: 600px;
-            width: 100%;
-          }}
-          h1 {{
-            font-size: 24px;
-            color: #ffffff;
-            margin-bottom: 20px;
-          }}
-          p {{
-            font-size: 16px;
-            color: #bbbbbb;
-            margin-bottom: 30px;
-          }}
-          .message__code{{
-            color: #bbbbbb;
-            font-size: 24px;
-            word-spacing: 5px;
-          }}
-        </style>
-      </head>
-      <body>
-        <div class='container'>
-          <div class='content'>
-            <h1>Welcome to DevHub!</h1>
-            <p>Please,copy this code and paste it in verification page</p>
-            <span class='message__code'>{code}</span>
-          </div>
-        </div>
-      </body>
-    </html>";
       var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
       var response = await client.SendEmailAsync(msg);
    }
