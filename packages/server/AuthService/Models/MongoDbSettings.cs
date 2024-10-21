432c5372442248430a30b7d1dc793a581f23eeca
Artem <temaprgod8@gmail.com>
2024-10-21 13:38:43 +0300

    fix: fixes in service Models and connections

--- packages/server/AuthService/Models/MongoDbSettings.cs
+++ packages/server/AuthService/Models/MongoDbSettings.cs
@@ -2,6 +2,7 @@
 
 public class MongoDbSettings
 {
-   public string ConnectionUri { get; set; }
-   public string DatabaseName { get; set; }
+   public string ConnectionUri { get; set; } = null!;
+   public string DatabaseName { get; set; } = null!;
+   public string CollectionName { get; set; } = null!;
 }
--- packages/server/AuthService/Models/User.cs
+++ packages/server/AuthService/Models/User.cs
@@ -1,4 +1,6 @@
-﻿using MongoDB.Bson;
+﻿using System.Text.Json.Serialization;
+using AuthService.Models.Enums;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace AuthService.Models;
@@ -8,13 +10,39 @@ public class User
 {
    [BsonId]
    [BsonRepresentation(BsonType.ObjectId)]
-   public string id { get; set; }
-   public string name { get; set; }
-   public string userName { get; set; }
-   public string avatar { get; set; }
+   [BsonElement("id")] 
+   [JsonPropertyName("id")]
+   public string Id { get; set; }
+   
+   [BsonElement("name")] 
+   public string Name { get; set; }
+   
+   [BsonElement("username")] 
+   public string UserName { get; set; }
+   
+   [BsonElement("password")]
+   public string Password { get; set; }
+   
+   [BsonElement("avatar")] 
+   public string Avatar { get; set; }
+   
+   [BsonElement("email")] 
+   public string Email { get; set; }
+   
+   [BsonElement("createdAt")] 
    [BsonRepresentation(BsonType.DateTime)]
-   public DateTime createdAt { get; set; } = DateTime.Now;
-   public int devPoints { get; set; } = 0;
-   public string activationLink { get; set; }
-   public bool isActivated { get; set; }
+   public DateTime CreatedAt { get; set; } = DateTime.Now;
+   
+   [BsonElement("devPoints")] 
+   public int DevPoints { get; set; } = 0;
+   
+   [BsonElement("activationCode")]
+   public string? ActivationCode { get; set; }
+   
+   [BsonElement("isActivated")]
+   public bool IsActivated { get; set; } = false;
+
+   [BsonElement("roles")]
+   [BsonRepresentation(BsonType.String)]
+   public List<UserRole> UserRole { get; set; } = new List<UserRole> { Enums.UserRole.User };
 }
