fe4c5fac51dc37f2282978af0af43516103c18a4
Milena Tsymbal <cymbalale13@gmail.com>
2024-10-23 00:13:58 +0300

    ref: [27] primary button's reusability is added

--- packages/mobile/app/src/main/java/com/devhub/devhubapp/PrimaryButtonFragment.kt
+++ packages/mobile/app/src/main/java/com/devhub/devhubapp/PrimaryButtonFragment.kt
@@ -5,15 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 class PrimaryButtonFragment : Fragment() {
+
+    private lateinit var button: Button
+    private var text: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_primary_button, container, false)
+        val view = inflater.inflate(R.layout.fragment_primary_button, container, false)
+
+        button = view.findViewById(R.id.primarybtn)
+        button.text = this.text
+
+        return view
     }
 
+    fun setButtonText(text: String) {
+        this.text = text
+    }
 }
