599c0ccd9e96e0624be40fa0333803eb595cba2b
ShutSasha <sanyashut159@gmail.com>
2024-10-24 19:00:21 +0300

    feat(verify-email): [22] added manual logic

--- packages/web/src/pages/confirm-email/confirm-email.page.tsx
+++ packages/web/src/pages/confirm-email/confirm-email.page.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { useVerifyEmailMutation } from '@api/auth.api'
 import { useAppSelector } from '@app/store/store'
 import { AuthBtn } from '@shared/components/auth/btn/btn.component'
@@ -8,10 +9,10 @@ import { AuthTitle } from '@shared/components/auth/title/auth-title.component'
 import { ErrorSpan } from '@shared/components/errors/error-span.component'
 import { AuthLayout } from '@shared/layouts/auth/auth.layout'
 import { handleServerException } from '@utils/handleServerException.util'
+import loaderGif from '@assets/gif/loading.gif'
+import { ROUTES } from '@pages/router/routes.enum'
 
 import { ErrorException } from '~types/error/error.type'
-import { useNavigate } from 'react-router-dom'
-import { ROUTES } from '@pages/router/routes.enum'
 
 export const ConfirmEmail = () => {
   const navigate = useNavigate()
@@ -22,8 +23,7 @@ export const ConfirmEmail = () => {
 
   const handleConfirm = async (): Promise<void> => {
     try {
-      const res = await verifyEmail({ email, activationCode: code }).unwrap()
-      console.log('response', res)
+      await verifyEmail({ email, activationCode: code }).unwrap()
 
       navigate(ROUTES.HOME)
     } catch (e) {
@@ -35,19 +35,23 @@ export const ConfirmEmail = () => {
     setCode(e.target.value)
   }
 
-  // const errors: string[] | undefined = handleServerException(error as ErrorException)
+  const errors: string[] | undefined = handleServerException(error as ErrorException)
 
   return (
     <AuthLayout>
       <AuthDevhubTitle />
       <AuthTitle title="Code sent to the email" style={{ marginBottom: '16px', textAlign: 'center' }} />
-      <AuthInput
-        placeholder="6-digital code"
-        value={code}
-        handleInput={handleChangeInput}
-        style={{ marginBottom: '16px' }}
-      />
-      {/* <div>{errors && errors.map((error: string, index: number) => <ErrorSpan key={index} value={error} />)}</div> */}
+      {isLoading ? (
+        <img src={loaderGif} />
+      ) : (
+        <AuthInput
+          placeholder="6-digital code"
+          value={code}
+          handleInput={handleChangeInput}
+          style={{ marginBottom: '16px' }}
+        />
+      )}
+      <div>{errors && errors.map((error: string, index: number) => <ErrorSpan key={index} value={error} />)}</div>
       <AuthBtn handleClick={handleConfirm} text="Confirm" />
     </AuthLayout>
   )
