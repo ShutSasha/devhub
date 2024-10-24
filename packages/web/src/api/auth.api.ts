d788797c22d1527d1998e9c6c7d5b8a72bc73911
ShutSasha <sanyashut159@gmail.com>
2024-10-24 18:19:11 +0300

    feat(verify-email): [22] added api

--- packages/web/src/api/auth.api.ts
+++ packages/web/src/api/auth.api.ts
@@ -2,15 +2,24 @@ import { createApi } from '@reduxjs/toolkit/query/react'
 
 import baseQueryWithReauth from './baseQueryWithReauth'
 
-import { SignUpDto } from '~types/user/user.dto'
+import { SignUpDto } from '~types/auth/sign-up.dto'
+import { IUser } from '~types/user/user.type'
+import { VerifyEmailDto, VerifyEmailResponse } from '~types/auth/verify-email.type'
 
 export const api = createApi({
   reducerPath: 'authApi',
   baseQuery: baseQueryWithReauth,
   endpoints: builder => ({
-    register: builder.mutation<any, SignUpDto>({
+    register: builder.mutation<IUser, SignUpDto>({
       query: body => ({
         url: 'auth/register',
+        method: 'POST',
+        body,
+      }),
+    }),
+    verifyEmail: builder.mutation<VerifyEmailResponse, VerifyEmailDto>({
+      query: body => ({
+        url: 'auth/verify-email',
         method: 'POST',
         body,
       }),
@@ -18,4 +27,4 @@ export const api = createApi({
   }),
 })
 
-export const { useRegisterMutation } = api
+export const { useRegisterMutation, useVerifyEmailMutation } = api
