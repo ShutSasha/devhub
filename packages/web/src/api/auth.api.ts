4a3161dcb56185e381dffd28cfb4c8c0ea182738
ShutSasha <sanyashut159@gmail.com>
2024-10-24 17:33:02 +0300

    chore: [22] removed unused var

--- packages/web/src/api/auth.api.ts
+++ packages/web/src/api/auth.api.ts
@@ -2,7 +2,6 @@ import { createApi } from '@reduxjs/toolkit/query/react'
 
 import baseQueryWithReauth from './baseQueryWithReauth'
 
-import { IUser } from '~types/user/user.type'
 import { SignUpDto } from '~types/user/user.dto'
 
 export const api = createApi({
