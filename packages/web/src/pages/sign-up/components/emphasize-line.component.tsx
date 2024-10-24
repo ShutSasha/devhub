667f2f297fc74b56a361f1d61b0be8556d5decb0
ShutSasha <sanyashut159@gmail.com>
2024-10-24 17:34:22 +0300

    feat(sign-up-page): [22] implemented manual logic

---
+++ packages/web/src/pages/sign-up/components/emphasize-line.component.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const StyledHr = styled.hr`
+  color: red;
+  background-color: rgba(48, 64, 80, 0.15);
+  height: 1px;
+  border: none;
+  margin-bottom: 16px;
+`
+
+export const EmphasizeLine = () => <StyledHr />
--- packages/web/src/shared/components/auth/input/auth-input.component.tsx
+++ packages/web/src/shared/components/auth/input/auth-input.component.tsx
@@ -35,14 +35,21 @@ interface AuthInputProps {
   type?: string
   placeholder?: string
   style?: CSSProperties
+  value?: string | undefined
   handleInput?: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export const AuthInput: FC<AuthInputProps> = ({ label, type, placeholder, style, handleInput }) => {
+export const AuthInput: FC<AuthInputProps> = ({ label, type, placeholder, style, value, handleInput }) => {
   return (
     <InputContainer>
       <Span>{label}</Span>
-      <Input type={type || 'text'} placeholder={placeholder} style={{ ...style }} onChange={handleInput} />
+      <Input
+        type={type || 'text'}
+        placeholder={placeholder}
+        style={{ ...style }}
+        value={value}
+        onChange={handleInput}
+      />
     </InputContainer>
   )
 }
