4efebcf38c77c72be73eadd031fae612c6910757
Artem <temaprgod8@gmail.com>
2024-10-30 14:42:13 +0200

    hotfix: [55] add password length checking

--- packages/server/AuthService/Contracts/Email/ChangePasswordRequest.cs
+++ packages/server/AuthService/Contracts/Email/ChangePasswordRequest.cs
@@ -9,6 +9,7 @@ public class ChangePasswordRequest
    public string Email { get; set; }
       
    [Required]
+   [StringLength(20,ErrorMessage ="{0} length must be more than {2} and less than {1} characters", MinimumLength = 8) ]
    public string Password { get; set; }
 
    [Required]
