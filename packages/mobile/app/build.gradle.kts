56a723163369b0febd938019a4ad51f6420a80b1
Milena Tsymbal <cymbalale13@gmail.com>
2024-10-31 01:44:54 +0200

    feat: [29] binding and retrofit, gson libraries are added

--- packages/mobile/app/build.gradle.kts
+++ packages/mobile/app/build.gradle.kts
@@ -17,6 +17,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -55,4 +59,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
 }
