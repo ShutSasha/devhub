64f57682747e2ff9b13ea26d00b98401bc5604e8
ShutSasha <sanyashut159@gmail.com>
2024-10-30 21:48:47 +0200

    feat: [67] added main layout

---
+++ packages/web/src/shared/layouts/main.layout.tsx
@@ -0,0 +1,37 @@
+import { FC, ReactNode } from 'react'
+import styled from 'styled-components'
+import { Header } from '@shared/components/header/header.component'
+import { colors } from '@shared/consts/colors.const'
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  color: ${colors.textPrimary};
+  background-color: ${colors.bgPage};
+`
+
+const Container = styled.div`
+  max-width: 1440px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 10px;
+
+  display: grid;
+  grid-template-columns: 300px minmax(300px, 1fr) 300px;
+  gap: 36px;
+`
+
+interface MainLayoutProps {
+  children: ReactNode
+}
+
+export const MainLayout: FC<MainLayoutProps> = ({ children }) => {
+  return (
+    <Wrapper>
+      <Header />
+      <Container>{children}</Container>
+      <p style={{ marginTop: 'auto' }}>footer</p>
+    </Wrapper>
+  )
+}
