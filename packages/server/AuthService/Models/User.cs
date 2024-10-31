49c31d84de56df9e95113bd0c3a6af0372834cf2
Artem <temaprgod8@gmail.com>
2024-10-31 03:28:30 +0200

    fix: fixed time

--- packages/server/AuthService/Models/User.cs
+++ packages/server/AuthService/Models/User.cs
@@ -30,8 +30,7 @@ public class User
    public string Email { get; set; }
    
    [BsonElement("createdAt")] 
-   [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
-   public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
+   public string CreatedAt { get; set; } = DateTime.UtcNow.ToString("o");
    
    [BsonElement("devPoints")] 
    public int DevPoints { get; set; } = 0;
