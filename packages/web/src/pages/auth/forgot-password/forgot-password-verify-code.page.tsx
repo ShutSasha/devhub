25876ebd21449551b6a7de67a8ac2900bf6d1c09
ShutSasha <sanyashut159@gmail.com>
2024-10-31 16:46:46 +0200

    feat: [54] implemented forgot-password logic

--- packages/web/src/pages/auth/forgot-password/forgot-password-verify-code.page.tsx
+++ packages/web/src/pages/auth/forgot-password/forgot-password-verify-code.page.tsx
@@ -1,39 +1,38 @@
-import { ChangeEvent } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useVerifyEmailMutation } from '@api/auth.api'
+import { useAppSelector } from '@app/store/store'
+import { AuthBtn } from '@shared/components/auth/btn/btn.component'
 import { AuthDevhubTitle } from '@shared/components/auth/devhub-title/auth-devhub-title.components'
+import { AuthInput } from '@shared/components/auth/input/auth-input.component'
 import { AuthTitle } from '@shared/components/auth/title/auth-title.component'
+import { ErrorSpan } from '@shared/components/errors/error-span.component'
 import { AuthLayout } from '@shared/layouts/auth/auth.layout'
 import { handleServerException } from '@utils/handleServerException.util'
 import loaderGif from '@assets/gif/loading.gif'
-import { AuthInput } from '@shared/components/auth/input/auth-input.component'
-import { ErrorSpan } from '@shared/components/errors/error-span.component'
-import { AuthBtn } from '@shared/components/auth/btn/btn.component'
-import { usePasswordVerificationCodeMutation } from '@api/auth.api'
-import { useAppDispatch, useAppSelector } from '@app/store/store'
-import { setForgetPasswordEmail } from '@features/auth/auth.slice'
 import { ROUTES } from '@pages/router/routes.enum'
 
 import { ErrorException } from '~types/error/error.type'
 
-export const ForgotPassword = () => {
+export const ForgotPasswordVerifyCode = () => {
   const navigate = useNavigate()
-  const [passwordVetificationCode, { isLoading, error }] = usePasswordVerificationCodeMutation()
 
   const email = useAppSelector(state => state.authSlice.forgetPasswordEmail)
-  const dispatch = useAppDispatch()
+  const [verifyEmail, { isLoading, error }] = useVerifyEmailMutation()
+  const [code, setCode] = useState<string>('')
 
-  const handleSendCode = async (): Promise<void> => {
+  const handleConfirm = async (): Promise<void> => {
     try {
-      await passwordVetificationCode({ email }).unwrap()
+      await verifyEmail({ email, activationCode: code }).unwrap()
 
-      navigate(ROUTES.FORGOT_PASSWORD_VERIFY)
+      navigate(ROUTES.ENTER_NEW_PASSWORD)
     } catch (e) {
       console.error(e)
     }
   }
 
   const handleChangeInput = (e: ChangeEvent<HTMLInputElement>): void => {
-    dispatch(setForgetPasswordEmail(e.target.value))
+    setCode(e.target.value)
   }
 
   const errors: string[] | undefined = handleServerException(error as ErrorException)
@@ -45,10 +44,15 @@ export const ForgotPassword = () => {
       {isLoading ? (
         <img src={loaderGif} />
       ) : (
-        <AuthInput label="Email" value={email} handleInput={handleChangeInput} style={{ marginBottom: '16px' }} />
+        <AuthInput
+          placeholder="6-digital code"
+          value={code}
+          handleInput={handleChangeInput}
+          style={{ marginBottom: '16px' }}
+        />
       )}
       <div>{errors && errors.map((error: string, index: number) => <ErrorSpan key={index} value={error} />)}</div>
-      <AuthBtn handleClick={handleSendCode} text="Send code" />
+      <AuthBtn handleClick={handleConfirm} text="Confirm" />
     </AuthLayout>
   )
 }
