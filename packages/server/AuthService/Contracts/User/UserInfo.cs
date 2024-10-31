bc8545518ceaec40c2668d45b76ea15dae8e4829
Artem <temaprgod8@gmail.com>
2024-10-31 20:50:11 +0200

    feat: [68] add contracts

---
+++ packages/server/AuthService/Contracts/User/UserInfo.cs
@@ -0,0 +1,12 @@
+namespace AuthService.Contracts.User;
+
+public class UserInfo
+{
+   public string sub { get; set; }
+   public string name { get; set; }
+   public string given_name { get; set; }
+   public string family_name { get; set; }
+   public string picture { get; set; }
+   public string email { get; set; }
+   public bool email_verified { get; set; }
+}
