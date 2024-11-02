8768599328f10fefb737f5a3288ac70dca4bec4d
Milena Tsymbal <cymbalale13@gmail.com>
2024-11-02 13:36:59 +0200

    fix: [74] change response data type

--- packages/mobile/app/src/main/java/com/devhub/devhubapp/api/AuthAPI.kt
+++ packages/mobile/app/src/main/java/com/devhub/devhubapp/api/AuthAPI.kt
@@ -2,28 +2,28 @@ package com.devhub.devhubapp.api
 
 import com.devhub.devhubapp.dataClasses.LoginResponse
 import com.devhub.devhubapp.dataClasses.TokenResponse
-import com.devhub.devhubapp.dataClasses.UserRegistrationRequest
+import com.devhub.devhubapp.dataClasses.RegistrationRequest
 import com.devhub.devhubapp.dataClasses.User
-import com.devhub.devhubapp.dataClasses.UserLoginRequest
+import com.devhub.devhubapp.dataClasses.LoginRequest
+import com.devhub.devhubapp.dataClasses.RegistrationResponse
 import com.devhub.devhubapp.dataClasses.VerifyEmailRequest
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
-import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface AuthAPI {
 
     @POST("auth/register")
-    fun register(@Body user: UserRegistrationRequest): Call<User>
+    fun register(@Body user: RegistrationRequest): Call<RegistrationResponse>
 
     @POST("auth/verify-email")
-    fun verifyEmail(@Body user: VerifyEmailRequest): Call<User>
+    fun verifyEmail(@Body user: VerifyEmailRequest): Call<ResponseBody>
 
     @POST("auth/login")
-    fun login(@Body user: UserLoginRequest): Call<LoginResponse>
+    fun login(@Body user: LoginRequest): Call<LoginResponse>
 
     @POST("auth/refresh")
     fun refreshToken(
