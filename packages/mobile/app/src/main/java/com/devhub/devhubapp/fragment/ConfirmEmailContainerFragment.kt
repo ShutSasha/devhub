12952dd5427ec08e948aae327a7f632dc22a2280
Milena Tsymbal <cymbalale13@gmail.com>
2024-11-02 13:44:57 +0200

    feat: [74] add verify email logic

--- packages/mobile/app/src/main/java/com/devhub/devhubapp/fragment/ConfirmEmailContainerFragment.kt
+++ packages/mobile/app/src/main/java/com/devhub/devhubapp/fragment/ConfirmEmailContainerFragment.kt
@@ -8,20 +8,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.marginBottom
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.devhub.devhubapp.R
-import com.devhub.devhubapp.activity.ConfirmEmailActivity
 import com.devhub.devhubapp.activity.WelcomeActivity
 import com.devhub.devhubapp.api.AuthAPI
+import com.devhub.devhubapp.classes.EncryptedPreferencesManager
 import com.devhub.devhubapp.classes.RetrofitClient
 import com.devhub.devhubapp.dataClasses.ErrorResponse
-import com.devhub.devhubapp.dataClasses.User
-import com.devhub.devhubapp.dataClasses.UserRegistrationRequest
 import com.devhub.devhubapp.dataClasses.VerifyEmailRequest
 import com.devhub.devhubapp.databinding.FragmentConfirmEmailContainerBinding
-import com.devhub.devhubapp.databinding.FragmentLoginContainerBinding
 import com.google.gson.Gson
 import okhttp3.ResponseBody
 import retrofit2.Call
@@ -31,15 +29,19 @@ import retrofit2.Response
 
 class ConfirmEmailContainerFragment : Fragment() {
 
+    private lateinit var encryptedPreferencesManager: EncryptedPreferencesManager
     private lateinit var authAPI: AuthAPI
     private lateinit var binding: FragmentConfirmEmailContainerBinding
     private var codeInput: String = ""
+    val title = TitleFragment()
     val activationError = ErrorFragment()
+    val primaryButtonFragment = PrimaryButtonFragment()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        encryptedPreferencesManager = EncryptedPreferencesManager(requireContext())
         authAPI = RetrofitClient.getInstance(requireContext()).getRetrofit().create(AuthAPI::class.java)
         binding =  FragmentConfirmEmailContainerBinding.inflate(layoutInflater, container, false)
 
@@ -50,11 +52,11 @@ class ConfirmEmailContainerFragment : Fragment() {
 
     private fun setUpFragment(){
         binding.activationError.visibility = View.GONE
+        binding.result.visibility = View.GONE
 
         val fragmentManager : FragmentManager = childFragmentManager
         val fragmentTransaction : FragmentTransaction = fragmentManager.beginTransaction()
 
-        val title = TitleFragment()
         title.setShowBackArrow(true)
         title.setTitleText("Confirming email")
         fragmentTransaction.add(R.id.title, title)
@@ -68,32 +70,26 @@ class ConfirmEmailContainerFragment : Fragment() {
                 }
             })
         }
-        fragmentTransaction.add(R.id.code_input_container, codeInputFragment)
+        fragmentTransaction.add(R.id.codeInputContainer, codeInputFragment)
 
         fragmentTransaction.add(R.id.activationError, activationError)
 
-        val primaryButtonFragment = PrimaryButtonFragment()
         primaryButtonFragment.setButtonText("Next")
         fragmentTransaction.add(R.id.primary_button_container, primaryButtonFragment)
-        primaryButtonFragment.setButtonAction { ConfirmEmail()}
+        primaryButtonFragment.setButtonAction { VerifyEmail()}
 
         fragmentTransaction.commit()
     }
 
-    private fun ConfirmEmail(){
-        val email = arguments?.getString("email")
-        if (email.isNullOrEmpty()) {
-            Log.e("ConfirmEmail", "Email is null or empty")
-            return
-        }
+    private fun VerifyEmail(){
 
         val verifyEmailRequest = VerifyEmailRequest(
-            email = email,
-            code = codeInput
+            email = encryptedPreferencesManager.getData("email") ?: "",
+            activationCode = codeInput
         )
 
-        authAPI.verifyEmail(verifyEmailRequest).enqueue(object : Callback<User> {
-            override fun onResponse(call: Call<User>, response: Response<User>) {
+        authAPI.verifyEmail(verifyEmailRequest).enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
                 if (response.isSuccessful) {
                     val verifyEmailResponse = response.body()
 
@@ -102,8 +98,7 @@ class ConfirmEmailContainerFragment : Fragment() {
                         Log.i("VerifyEmail", "VerifyEmail Successful")
                         Log.d("Response", "Response received: ${response.code()}")
 
-                        val intent = Intent(requireContext(), WelcomeActivity::class.java)
-                        startActivity(intent)
+                        onSuccess()
                     }
 
                 } else {
@@ -112,8 +107,9 @@ class ConfirmEmailContainerFragment : Fragment() {
                 }
             }
 
-            override fun onFailure(call: Call<User>, t: Throwable) {
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
                 Log.e("VerifyEmail", "Failed: ${t.message}")
+                onError()
             }
         })
     }
@@ -139,6 +135,56 @@ class ConfirmEmailContainerFragment : Fragment() {
                 Log.e("Error Parsing", "Error parsing error response: ${e.message}")
             }
         }
+    }
+
+    private fun onSuccess() {
+
+        binding.codeInputContainer.visibility = View.GONE
+        binding.tvSubtitle.visibility = View.GONE
+        binding.activationError.visibility = View.GONE
+
+        val layoutParams = binding.title.layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.bottomMargin = 0
+        binding.title.layoutParams = layoutParams
+
+        val color = ContextCompat.getColor(requireContext(), R.color.success)
+        title.setTextColour(color)
+
+        binding.result.setTextColor(color)
+        binding.result.text = "Your email was successfully confirmed!"
+        binding.result.visibility = View.VISIBLE
+
+        primaryButtonFragment.setButtonText("Confirm")
+        primaryButtonFragment.setButtonAction {
+            val intent = Intent(requireContext(), WelcomeActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun onError() {
+
+        binding.codeInputContainer.visibility = View.GONE
+        binding.tvSubtitle.visibility = View.GONE
+        binding.activationError.visibility = View.GONE
+
+        val layoutParams = binding.title.layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.bottomMargin = 0
+        binding.title.layoutParams = layoutParams
+
+        val color = ContextCompat.getColor(requireContext(), R.color.wrong)
+        title.setTextColour(color)
+
+        binding.result.setTextColor(color)
+        binding.result.text = "Something went wrong, please try again"
+        binding.result.visibility = View.VISIBLE
+
+        primaryButtonFragment.setButtonText("Back")
+        primaryButtonFragment.setButtonAction {
+            val intent = Intent(requireContext(), WelcomeActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 
 }
