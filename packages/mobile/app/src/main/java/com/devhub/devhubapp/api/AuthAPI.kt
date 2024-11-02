d2aab4cc984437f710ed3ccb404a034fcbbaf5b1
Milena Tsymbal <cymbalale13@gmail.com>
2024-11-03 01:50:44 +0200

    feat: [71] add password change api

--- packages/mobile/app/src/main/java/com/devhub/devhubapp/api/AuthAPI.kt
+++ packages/mobile/app/src/main/java/com/devhub/devhubapp/api/AuthAPI.kt
@@ -1,10 +1,12 @@
 package com.devhub.devhubapp.api
 
+import com.devhub.devhubapp.dataClasses.ChangePasswordRequest
 import com.devhub.devhubapp.dataClasses.LoginResponse
 import com.devhub.devhubapp.dataClasses.TokenResponse
 import com.devhub.devhubapp.dataClasses.RegistrationRequest
 import com.devhub.devhubapp.dataClasses.User
 import com.devhub.devhubapp.dataClasses.LoginRequest
+import com.devhub.devhubapp.dataClasses.PasswordVerificationRequest
 import com.devhub.devhubapp.dataClasses.RegistrationResponse
 import com.devhub.devhubapp.dataClasses.VerifyEmailRequest
 import okhttp3.ResponseBody
@@ -12,18 +14,25 @@ import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 
 interface AuthAPI {
 
     @POST("auth/register")
-    fun register(@Body user: RegistrationRequest): Call<RegistrationResponse>
+    fun register(@Body registrationRequest: RegistrationRequest): Call<RegistrationResponse>
 
     @POST("auth/verify-email")
-    fun verifyEmail(@Body user: VerifyEmailRequest): Call<ResponseBody>
+    fun verifyEmail(@Body verifyEmailRequest: VerifyEmailRequest): Call<ResponseBody>
 
     @POST("auth/login")
-    fun login(@Body user: LoginRequest): Call<LoginResponse>
+    fun login(@Body loginRequest: LoginRequest): Call<LoginResponse>
+
+    @PATCH("auth/password-verification-code")
+    fun passwordVerificationCode(@Body passwordVerificationRequest: PasswordVerificationRequest): Call<ResponseBody>
+
+    @PATCH("auth/change-password")
+    fun changePassword(@Body changePassword: ChangePasswordRequest): Call<ResponseBody>
 
     @POST("auth/refresh")
     fun refreshToken(
