28921abc40d5c52316e6f1f5bc3b8d5b05d1f241
Yaroslav Ivanov <yaroslavivanov2005@gmail.com>
2024-11-03 23:18:58 +0100

    feat: [36] updated redirected user fields to header

--- packages/mobile/app/src/main/java/com/devhub/devhubapp/fragment/HeaderFragment.kt
+++ packages/mobile/app/src/main/java/com/devhub/devhubapp/fragment/HeaderFragment.kt
@@ -1,5 +1,6 @@
 package com.devhub.devhubapp.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +9,7 @@ import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.devhub.devhubapp.R
-
+import com.devhub.devhubapp.activity.CreatePostActivity
 
 class HeaderFragment : Fragment() {
 
@@ -18,15 +19,23 @@ class HeaderFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_header, container, false)
 
-
         val avatarImageView = view.findViewById<ImageView>(R.id.user_avatar)
+        val createPostButton = view.findViewById<ImageView>(R.id.create_post_button)
+
         val userAvatarUrl = arguments?.getString("USER_AVATAR")
+        val username = arguments?.getString("USERNAME")
 
         if (!userAvatarUrl.isNullOrEmpty()) {
             Glide.with(this)
                 .load(userAvatarUrl)
-                .placeholder(R.drawable.people_brainstorming_idea)
                 .into(avatarImageView)
+        }
+
+        createPostButton.setOnClickListener {
+            val intent = Intent(requireContext(), CreatePostActivity::class.java)
+            intent.putExtra("USER_AVATAR", userAvatarUrl)
+            intent.putExtra("USERNAME", username)
+            startActivity(intent)
         }
 
         return view
