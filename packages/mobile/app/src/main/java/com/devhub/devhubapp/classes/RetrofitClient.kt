385c7ea823f4c11dab51cbfbd32e076596a29b94
Yaroslav Ivanov <yaroslavivanov2005@gmail.com>
2024-11-18 15:37:02 +0100

    feat: [86] updated retrofit client

--- packages/mobile/app/src/main/java/com/devhub/devhubapp/classes/RetrofitClient.kt
+++ packages/mobile/app/src/main/java/com/devhub/devhubapp/classes/RetrofitClient.kt
@@ -9,6 +9,7 @@ import com.devhub.devhubapp.api.PostAPI
 import com.devhub.devhubapp.interceptors.AuthInterceptor
 import com.google.gson.GsonBuilder
 import java.util.Date
+import java.util.concurrent.TimeUnit
 
 class RetrofitClient private constructor(context: Context) {
     private val BASE_URL = "http://10.0.2.2:5295/api/"
@@ -23,21 +24,27 @@ class RetrofitClient private constructor(context: Context) {
             .registerTypeAdapter(Date::class.java, DateDeserializer())
             .create()
 
-        retrofit = Retrofit.Builder()
+        val baseRetrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
 
-        authAPI = retrofit.create(AuthAPI::class.java)
-        postAPI = retrofit.create(PostAPI::class.java)
+        authAPI = baseRetrofit.create(AuthAPI::class.java)
 
         val client = OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
             .addInterceptor(AuthInterceptor(authAPI, encryptedPreferencesManager))
             .build()
 
-        retrofit = retrofit.newBuilder()
+        retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .client(client)
             .build()
+
+        postAPI = retrofit.create(PostAPI::class.java)
     }
 
     fun getRetrofit(): Retrofit {
