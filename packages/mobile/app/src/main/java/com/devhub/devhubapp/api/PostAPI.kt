70b3e24beabfc6964c5ccbcd8ce9a54db905ad00
Yaroslav Ivanov <yaroslavivanov2005@gmail.com>
2024-11-20 04:32:00 +0100

    feat: [103] implemented api for adding likes and dislikes

--- packages/mobile/app/src/main/java/com/devhub/devhubapp/api/PostAPI.kt
+++ packages/mobile/app/src/main/java/com/devhub/devhubapp/api/PostAPI.kt
@@ -1,13 +1,17 @@
 package com.devhub.devhubapp.api
 
 import com.devhub.devhubapp.dataClasses.Post
+import com.devhub.devhubapp.dataClasses.UserIdRequest
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PostAPI {
@@ -25,5 +29,17 @@ interface PostAPI {
         @Part("content") content: RequestBody,
         @Part("tags") tags: RequestBody,
         @Part headerImage: MultipartBody.Part?
+    ): Call<Post>
+
+    @POST("posts/{id}/like")
+    fun likePost(
+        @Path("id") postId: String,
+        @Body requestBody: UserIdRequest
+    ): Call<Post>
+
+    @POST("posts/{id}/dislike")
+    fun dislikePost(
+        @Path("id") postId: String,
+        @Body requestBody: UserIdRequest
     ): Call<Post>
 }
