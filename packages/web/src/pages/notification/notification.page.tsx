ad2b7b0067861f0da58762c3238b8b86e9dd8cfc
ShutSasha <sanyashut159@gmail.com>
2024-12-21 19:17:52 +0200

    feat(web): added read notification logic

--- packages/web/src/pages/notification/notification.page.tsx
+++ packages/web/src/pages/notification/notification.page.tsx
@@ -1,14 +1,14 @@
 import { MainLayout } from '@shared/layouts/main.layout'
 import { ROUTES } from '@pages/router/routes.enum'
 import { useAppDispatch, useAppSelector } from '@app/store/store'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useLazyGetNotificationsByUserQuery, useReadNotificationByIdMutation } from '@api/notification.api'
 import { setNotifications } from '@features/notification/notifications.slice'
+import { colors } from '@shared/consts/colors.const'
 
 import * as _ from './notification.style'
 
 export const Notification = () => {
-  const { id } = useParams()
   const notifications = useAppSelector(state => state.notificationSlice.notifications)
   const dispatch = useAppDispatch()
   const [getNotifications] = useLazyGetNotificationsByUserQuery()
@@ -20,7 +20,7 @@ export const Notification = () => {
     navigate(ROUTES.USER_PROFILE.replace(':id', id))
   }
 
-  const handleReadNotification = async () => {
+  const handleReadNotification = async (id: string) => {
     try {
       await readNotificationById({ notification_id: id }).unwrap()
       const { data } = await getNotifications({ user_id: user?._id })
@@ -40,16 +40,20 @@ export const Notification = () => {
       <div></div>
       <div>
         <_.UsersList>
-          {notifications?.unread.length &&
-            notifications?.unread.map(notification => (
-              <_.UserListItem key={notification.id}>
-                <_.UserListItemDataContainer onClick={() => handleRedirectToUserProfile(notification.sender.id)}>
-                  <_.UserAvatar src={notification.sender.avatar} alt="avatar" />
-                  <_.UserName>{notification.sender.username}</_.UserName>
-                </_.UserListItemDataContainer>
-                <_.ReadNotificationIcon onClick={handleReadNotification} />
-              </_.UserListItem>
-            ))}
+          {notifications?.unread.length
+            ? notifications?.unread.map(notification => (
+                <_.UserListItem key={notification.id}>
+                  <_.UserListItemDataContainer onClick={() => handleRedirectToUserProfile(notification.sender.id)}>
+                    <_.UserAvatar src={notification.sender.avatar} alt="avatar" />
+                    <_.UserName>
+                      {notification.sender.username}{' '}
+                      <span style={{ color: colors.black300 }}>is now follow on you!</span>
+                    </_.UserName>
+                  </_.UserListItemDataContainer>
+                  <_.ReadNotificationIcon onClick={() => handleReadNotification(notification.id)} />
+                </_.UserListItem>
+              ))
+            : 'Notifications list is empty'}
         </_.UsersList>
       </div>
       <div></div>
